132a999e770757fb550a8d47e620b6de735f4d33
whenyd <whenyd@126.com>
2025-01-11 00:24:38 +0800

    2. PPM

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::io::{stdout, Write};
+
+fn main() {
+    /* Image */
+    let image_width = 256;
+    let image_height = 256;
+
+    /* Render */
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in 0..image_height {
+        eprint!("\rScanlines remaining: {}", image_height - j);
+        stdout().flush().unwrap();
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+    eprintln!("\rDone!                         ");
+}
