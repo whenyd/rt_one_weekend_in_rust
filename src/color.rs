a675802e2a799371a0c98a042e775d0a55058f48
whenyd <whenyd@126.com>
2025-01-15 00:39:32 +0800

    8.2.1 对最终的颜色使用 clamp 限制范围

--- src/color.rs
+++ src/color.rs
@@ -1,14 +1,19 @@
 use std::io::Write;
 
+use crate::interval::Interval;
+
 use super::vec3::Vec3;
 
 pub type Color = Vec3;
 
 impl Color {
     pub fn write_color(&self, out: &mut dyn Write) -> std::io::Result<()> {
+        // color 是多个像素求平均的结果, 所以需要 clamp 确保范围正确
+        let intensity = Interval::new(0.000, 0.999);
+
         write!(out, "{} {} {}\n",
-               (255.999 * self.x()) as i32,
-               (255.999 * self.y()) as i32,
-               (255.999 * self.z()) as i32)
+               (256.0 * intensity.clamp(self.x())) as i32,
+               (256.0 * intensity.clamp(self.y())) as i32,
+               (256.0 * intensity.clamp(self.z())) as i32)
     }
 }
